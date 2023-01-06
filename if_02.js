2dadd8641447c4b9eecd90a2284050642cb65d40
Pachecodb <56847008+Pachecodb@users.noreply.github.com>
2023-01-06 10:15:29 -0300

    Add files via upload

---
+++ if_02.js
@@ -0,0 +1,11 @@
+const nota = 7;
+const bomComportamento = false;
+
+if (nota >= 7 && bomComportamento) {
+    console.log("Parabens!");
+}
+
+if (nota < 7 || !bomComportamento) {
+    console.log("Uma pena");
+}
+console.log("Fim!");
